4344ce9b1b652f3c81b853f796d15653f347248b
ryota.kojima <user033.dounyu@techacademy.jp>
2024-05-22 11:55:16 +0900

    wordDTOの修正、indexServletの作成、雛形(app.jsp)の作成

---
+++ src/models/wordDTO.java
@@ -0,0 +1,92 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllDate",
+        query = "SELECT * FROM Word ORDER BY ID"
+    )
+})
+
+@Table(name = "Word")
+public class wordDTO {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "Name", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "Mean", nullable = false)
+    private String mean;
+
+    @Column(name = "Fragment", nullable = false)
+    private Integer fragment;
+
+    @Column(name = "Date", nullable = false)
+    private Timestamp date;
+
+    @Column(name = "CategoryID", nullable = true)
+    private Integer categoryId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMean() {
+        return mean;
+    }
+
+    public void setMean(String mean) {
+        this.mean = mean;
+    }
+
+    public Integer getFragment() {
+        return fragment;
+    }
+
+    public void setFragment(Integer fragment) {
+        this.fragment = fragment;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public Integer getCategoryID() {
+        return categoryId;
+    }
+
+    public void setCategory(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+}
