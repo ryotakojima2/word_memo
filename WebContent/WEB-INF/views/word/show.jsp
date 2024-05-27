b09abd04224f96fd1a173ed3ac73466065a36555
ryota.kojima <user033.dounyu@techacademy.jp>
2024-05-27 09:34:16 +0900

    ダウンロード済みファイルの更新

---
+++ WebContent/WEB-INF/views/word/show.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+
+<c:import url="../layout/app.jsp">
+    <c:param name="content">
+
+        <h2>id : ${word.id} のメッセージ詳細ページ</h2>
+
+        <p>単語：<c:out value="${word.name}" /></p>
+        <p>意味：<c:out value="${word.mean}" /></p>
+
+<c:if test="$word.fragment = 1}">
+    <p>学習済み</p>
+</c:if>
+<c:if test="${word.fragment = 0}">
+    <p>未学習</p>
+</c:if>
+
+        <p>作成日時：<fmt:formatDate value="${word.date}" pattern="yyyy-MM-dd HH:mm:ss" /></p>
+
+
+        <p><a href="$indexのリンクを書き込んでください">一覧に戻る</a></p>
+ <p><a href="$リンク">編集する</a></p>
+ <p><a href="$リンク">削除する</a></p>
+
+    </c:param>
+</c:import>
---
+++ src/controllers/UpdateServlet.java
@@ -0,0 +1,60 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.wordDTO;
+import util.DBUtil;
+
+/**
+ * Servlet implementation class UpdateServlet
+ */
+@WebServlet("/update")
+public class UpdateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
+            wordDTO u = em.find(wordDTO.class, (Integer)(request.getSession().getAttribute("word_id")));
+
+            // フォームの内容を各フィールドに上書き
+            String name = request.getParameter("name");
+            u.setName(name);
+
+            String mean = request.getParameter("mean");
+            u.setMean(mean);
+
+            // データベースを更新
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            em.close();
+
+            // セッションスコープ上の不要になったデータを削除
+            request.getSession().removeAttribute("word_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
+}
